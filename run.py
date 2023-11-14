05bb0c5c72f862bfe0dd1c3655f0b102f327b3eb
Søren Rasmussen <soren.fussingo@gmail.com>
2023-11-14 13:40:34 +0100

    Run-opdateret

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import Source.laesreportingdate as rdate
 
 #Gyldige arg.: Nat - Open - For"
 def run():
-    rdate.create_oversigt('open')
+    rdate.create_oversigt('For')
 
 if __name__ == '__main__':
     run()
